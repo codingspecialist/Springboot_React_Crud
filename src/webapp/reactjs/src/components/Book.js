99c5255cadada67a0ad6e9e643459742915f73a3
ssar <ssar@nate.com>
2020-09-01 14:56:40 +0900

    form태그로 책 전송 페이지 완성

--- src/webapp/reactjs/src/components/Book.js
+++ src/webapp/reactjs/src/components/Book.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Form, Button, Col } from 'react-bootstrap';
 
-const Book = () => {
+const Book = (props) => {
+
+	const [book, setBook] = useState({
+		title: '',
+		author: '',
+		coverPhotoURL: '',
+		isbnNumber: '',
+		price: '',
+		language: ''
+	});
+
+	const submitBook = (e) => {
+		alert(book.title);
+		e.preventDefault();
+	}
+
+	const bookChange = (e)=>{
+		setBook({
+			...book,
+			[e.target.name]:e.target.value
+		});
+		console.log(book.title);
+	}
+
 	return (
-		<div className="text-white">
-			 Add Book
-		</div>
+		<Card className={"border border-dark bg-dark text-white"}>
+			<Card.Header>Add Book</Card.Header>
+			<Form id="bookFormId" onSubmit={submitBook}>
+				<Card.Body> 
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="formGridTitle">
+							<Form.Label>Title</Form.Label>
+							<Form.Control required type="text" name="title" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book Title" />
+						</Form.Group>
+						<Form.Group as={Col} controlId="formGridAuthor">
+							<Form.Label>Author</Form.Label>
+							<Form.Control required type="text" name="author" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book Author" />
+						</Form.Group>
+					</Form.Row>
+
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="formGridCoverPhotoURL">
+							<Form.Label>Tover Photo URLitle</Form.Label>
+							<Form.Control required type="text" name="coverPhotoURL" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book Cover Photo URL" />
+						</Form.Group>
+						<Form.Group as={Col} controlId="formGridISBNNumber">
+							<Form.Label>ISBN Number</Form.Label>
+							<Form.Control required type="text" name="isbnNumber" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book ISBN Number" />
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="formGridPrice">
+							<Form.Label>Price</Form.Label>
+							<Form.Control required type="text" name="price" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book Price" />
+						</Form.Group>
+						<Form.Group as={Col} controlId="formGridLanguage">
+							<Form.Label>Language</Form.Label>
+							<Form.Control required type="text" name="language" onChange={bookChange} className={"bg-dark text-white"} placeholder="Enter Book Language" />
+						</Form.Group>
+					</Form.Row>
+
+				</Card.Body>
+				<Card.Footer style={{ "textAlign": "right" }}>
+					<Button size="sm" variant="success" type="submit">
+						Submit
+  					</Button>
+				</Card.Footer>
+			</Form>
+		</Card>
 	);
 };
 
--- src/webapp/reactjs/src/components/BookList.js
+++ src/webapp/reactjs/src/components/BookList.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Card, Table } from 'react-bootstrap';
 
 const BookList = () => {
 	return (
-		<div className="text-white">
-			 BookList
-		</div>
+		<Card className={"border border-dark bg-dark text-white"}>
+			<Card.Header>BookList</Card.Header>
+			<Card.Body>
+				<Table bordered hover striped variant="dark">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Author</th>
+							<th>ISBN Number</th>
+							<th>Price</th>
+							<th>Language</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr align="center">
+							<td colSpan="6">No Books Available.</td>
+						</tr>
+					</tbody>
+				</Table>
+			</Card.Body>
+		</Card>
 	);
 };
 
