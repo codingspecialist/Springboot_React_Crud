9d9e5b7885ad8ece35198caeeb99e7556d4fa5af
ssar <ssar@nate.com>
2020-09-01 16:13:53 +0900

    axios를 이용한 book 정보 리스트 뿌리기

--- src/webapp/reactjs/src/components/BookList.js
+++ src/webapp/reactjs/src/components/BookList.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import { Card, Table } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Card, Table, Image, ButtonGroup, Button } from 'react-bootstrap';
+import axios from 'axios';
 
-const BookList = () => {
+const BookList = (props) => {
+
+	const [books, setBooks] = useState([]);
+
+	const findAllBooks = () => {
+		axios.get("http://localhost:8081/rest/books")
+		.then(({data})=> setBooks(data));
+	}
+
+	// didMount(), didUpdate(), willUnMount()
+	useEffect(() => {
+		findAllBooks();
+	},[]);
+
 	return (
 		<Card className={"border border-dark bg-dark text-white"}>
 			<Card.Header>BookList</Card.Header>
@@ -18,9 +32,29 @@ const BookList = () => {
 						</tr>
 					</thead>
 					<tbody>
-						<tr align="center">
-							<td colSpan="6">No Books Available.</td>
-						</tr>
+						{books.length === 0 ? 
+							<tr align="center">
+								<td colSpan="6">{books.length} No Books Available.</td>
+							</tr>
+							:
+							books.map((book)=> (
+								<tr key={book.id}>
+									<td>
+										<Image src={book.coverPhotoURL} roundedCircle width={25} height={25}/> {book.title}
+									</td>
+									<td>{book.author}</td>
+									<td>{book.isbnNumber}</td>
+									<td>{book.price}</td>
+									<td>{book.language}</td>
+									<td>
+										<ButtonGroup>
+											<Button variant="warning" size="sm" >수정</Button>
+											<Button variant="danger" size="sm">삭제</Button>
+										</ButtonGroup>
+									</td>
+								</tr>
+							))
+						}
 					</tbody>
 				</Table>
 			</Card.Body>
